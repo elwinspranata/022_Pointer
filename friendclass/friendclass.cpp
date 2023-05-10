12aeb1310d26fae9bac78fcdac0c5feedff50465
elwinspranata <elwinpranata02@gmail.com>
2023-05-10 15:59:53 +0700

    frend

---
+++ friendclass/friendclass.cpp
@@ -0,0 +1,31 @@
+#include <iostream> 
+using namespace std; 
+
+class orang {
+
+private:
+	string nama;
+public:
+	void setnama(string pnama);
+	friend class siswa;
+};
+
+class siswa {
+private:
+	int id;
+public:
+	void setid(int pid);
+	void displayAll(orang& a);
+};
+
+
+void siswa::displayAll(orang& a) {
+	cout << id << endl << a.nama;
+};
+
+void siswa::setid(int pid) {
+	id = pid;
+}
+
+
+
