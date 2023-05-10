cb0e896b95d4305fce5816aa070af6ecec5bb0d8
elwinspranata <elwinpranata02@gmail.com>
2023-05-10 15:07:36 +0700

    pointer

--- 022_pointer/022_pointer.cpp
+++ 022_pointer/022_pointer.cpp
@@ -7,3 +7,18 @@ public:
 	void showNim(); //deklarasi method 
 };
 
+void mahasiswa::showNim() { //implementasi method diluar class 
+	cout << "no Induk = " << nim << endl;
+}
+
+int main()
+{
+	mahasiswa mhs{ 1 };     //object mhs 
+	mhs.showNim();          //Member Acces Operator 
+
+	mahasiswa ref = mhs;   //Pointer reference refmhs 
+	ref.nim = 2;           //member Acces Operator 
+	mhs.showNim(); 
+
+
+}
